b1c19d269768ba6aae0037d8370532a269d8a76a
Nihal Bhardwaj <bhardwajnihal00721@gmail.com>
2025-03-09 14:51:38 +0530

    introduced token authentication in the ws layer

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,15 +1,50 @@
-import { WebSocketServer } from 'ws';
+// Initializing the Websockect server
+// Defining the real time chatting and message broadcasting logic
 
-const wss = new WebSocketServer( {port : 8080} )
+import { WebSocketServer } from "ws";
+import jwt from 'jsonwebtoken'
+import {JWT_SECRET} from '@repo/common-configs/config'
 
-console.log("ws server active on port 8080");
+const wss = new WebSocketServer({port:8080});
+console.log("Websocket Server active on port 8080");
+
+// type for jwt token
+interface decodedToken{
+    userId : string
+}
+
+function verifyToken(token:string): string | null {
+
+    try {
+        const decoded = jwt.verify(token,JWT_SECRET as string) as decodedToken;
+        if(!decoded || !decoded.userId){
+            return null;
+        }
+        return decoded.userId
+
+    } catch (error) {
+        console.error("Token Validation failed!");
+        return null;
+    }
+}
+
+wss.on('connection', (ws, req) => {
+
+// verify token
+// ws doesn't support headers, so tokens are to be passed as queryparams
+   const params = new URLSearchParams(req.url?.split('?')[1]);              // way to extrats params from ws url
+   const token = params.get('token') || ""                          // url = ws://localhost:3001?token=${yourToken}
+   const VerifiedUser = verifyToken(token);
+   
+   if(VerifiedUser == null){
+    ws.close(4003, 'User not Authenticated!');                  // code convention in ws differ from http
+    return
+   }
 
 
-wss.on('connection' , (ws:any) => {
+// Authenticated operations from here
 
-    ws.on('message', (message:any) => {
+    ws.on('message', () => {
         ws.send("pong")
-        console.log(message);
-        
     })
 })
